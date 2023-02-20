42a185a82034019148c045e1d3c8f11710bb826b
Riccardo Nestola <riccardinux@gmail.com>
2023-02-20 17:45:24 +0100

    add route and migration

---
+++ database/migrations/2023_02_20_163100_create_projects_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 200)->unique();
+            $table->text("description");
+            $table->string('category', 100)->default('untitled category');
+            $table->integer('year');
+            $table->string('technology_used');
+            $table->string('thumb')->nullable();;
+            $table->timestamp('date_added')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
